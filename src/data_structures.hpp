6095b2c2524f009f915fe31cb81567054b5c8823
luchenps <49031284+luchenps@users.noreply.github.com>
2020-11-08 15:31:12 -0300

    new helper functions and debug prints

--- src/data_structures.hpp
+++ src/data_structures.hpp
@@ -21,6 +21,11 @@ namespace data_structures {
         FUNCTION,
     };
 
+    enum class assign_type : int {
+        LOCAL,
+        GLOBAL,
+    };
+
     class variable_data {
         int linenum;
         lua_things::Type type;
@@ -44,7 +49,9 @@ namespace data_structures {
 
         public:
         void add_var(const std::string& name, int linenum, lua_things::Type type) {
-            table.emplace(name, variable_data(linenum, type));
+            variable_data data{linenum, type};
+            // table.insert_or_assign(name, data);
+            table[name] = data;
         }
 
         bool lookup_var(const std::string& name) {
@@ -63,25 +70,49 @@ namespace data_structures {
         label_set goto_calls;
 
         scope(scope_type type) : type(type) {}
+
+        void add_label(const char* name) {
+            goto_labels.emplace(name);
+        }
+        void add_label(std::string& name) {
+            goto_labels.emplace(name);
+        }
     };
 
     struct context {
         std::vector<scope> scope_stack;
+        bool is_in_assignment;
+        // assign_type assignment_type;
+        // std::vector<std::string> assign_names;
+        // std::vector<lua_things::Type> assign_types;
 
-        bool verify_break() const {
+        bool verify_break() {
+            #ifdef DLVCDEBUG
+            std::cerr << "----- verify_break ------" << std::endl;
+            #endif
             for (auto iter = std::rbegin(scope_stack); iter != std::rend(scope_stack); ++iter) {
                 auto& s = *iter;
                 
                 if (s.type == scope_type::LOOP) {
+                    #ifdef DLVCDEBUG
+                    std::cerr << "SCOPE_TYPE: LOOP" << std::endl;
+                    #endif
                     return true;
                 } else if (s.type == scope_type::FUNCTION) {
+                    #ifdef DLVCDEBUG
+                    std::cerr << "SCOPE_TYPE: FUNCTION" << std::endl;
+                    #endif
                     return false;
                 }
             }
+
+            #ifdef DLVCDEBUG
+                std::cerr << "-------------------------------------------------" << std::endl;
+            #endif
             return false;
         }
 
-        bool verify_goto_calls() const {
+        bool verify_goto_calls() {
             label_set all_labels;
             label_set all_calls;
             for (const auto& s : scope_stack) {
@@ -99,6 +130,34 @@ namespace data_structures {
                 std::inserter(difference, std::begin(difference))
             );
             return difference.size() == 0;
+        }
+
+        scope& last_scope() {
+            return scope_stack.at(scope_stack.size() - 1);
+        }
+
+        scope& global_scope() {
+            return scope_stack.at(0);
+        }
+
+        void new_scope(scope_type t) {
+            #ifdef DLVCDEBUG
+            std::cerr << "----- new_scope ------" << std::endl;
+            #endif
+            scope_stack.emplace_back(t);
+            #ifdef DLVCDEBUG
+                std::cerr << "-------------------------------------------------" << std::endl;
+            #endif
+        }
+
+        void remove_scope() {
+            #ifdef DLVCDEBUG
+            std::cerr << "----- remove_scope ------" << std::endl;
+            #endif
+            scope_stack.pop_back();
+            #ifdef DLVCDEBUG
+                std::cerr << "-------------------------------------------------" << std::endl;
+            #endif
         }
     };
 };
