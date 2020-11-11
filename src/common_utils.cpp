6808e8b1bb866fc1db731285fe046b2c0281dcde
luchenps <49031284+luchenps@users.noreply.github.com>
2020-11-10 22:14:58 -0300

    global variables moved to a separated library

---
+++ src/common_utils.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include "data_structures.hpp"
+#include "lua_things.hpp"
+#include "common_utils.hpp"
+
+
+namespace global {
+    std::vector<std::string> namelist;
+    std::vector<lua_things::Type> explist;
+    data_structures::assign_type assign_type;
+    std::vector<std::tuple<std::string, var_type>> varlist;
+    std::string last_identifier;
+    std::vector<std::string> full_funcname;
+    list_type assign_list_type;
+    list_type prev_assign_list_type;
+    std::string for_init_id;
+}
---
+++ src/common_utils.hpp
@@ -0,0 +1,36 @@
+#ifndef common_utils_hpp_INCLUDED
+#define common_utils_hpp_INCLUDED
+
+#include <string>
+#include <vector>
+#include "data_structures.hpp"
+#include "lua_things.hpp"
+
+enum class var_type : int {
+    NAME,
+    INDEX,
+};
+
+enum class list_type : int {
+    VARLIST,
+    EXPLIST,
+    NO_TYPE,
+};
+
+namespace global {
+    /*
+     * Várias variáveis globais em um namespace para ter o mínimo de
+     * organização.
+     */
+    extern std::vector<std::string> namelist;
+    extern std::vector<lua_things::Type> explist;
+    extern data_structures::assign_type assign_type;
+    extern std::vector<std::tuple<std::string, var_type>> varlist;
+    extern std::string last_identifier;
+    extern std::vector<std::string> full_funcname;
+    extern list_type assign_list_type;
+    extern list_type prev_assign_list_type;
+    extern std::string for_init_id;
+}
+#endif // common_utils_hpp_INCLUDED
+
